2d984f0bfa869ec50f6f09e20de9497e5ffeb308
Bennert Depuydt <bennert.depuydt@student.odisee.be>
2021-02-23 16:26:49 +0100

    add working predictionintent.php

--- azure-luis-php/predictionintent.php
+++ azure-luis-php/predictionintent.php
@@ -22,13 +22,6 @@ $endpointKey = "";
 $endpoint = "";
 
 // The LUIS query term
-$term = "hi";
-
-https://testsbennert.cognitiveservices.azure.com/luis/prediction/v3.0/apps
-///fba6db0f-3d1d-42cd-8bda-488c14db85f6/slots/staging/predict
-//?subscription-key=9692289c484a4b0aa0e673b3a7169d1f&verbose=true
-//&show-all-intents=true&log=true&query=YOUR_QUERY_HERE
-
 function AnalyzeText($url, $app, $key, $query) {
     // Prepare HTTP request
     // NOTE: Use the key 'http' even if you are making an HTTPS request. See:
@@ -52,30 +45,32 @@ function AnalyzeText($url, $app, $key, $query) {
     );
 
     $url = $url . $app . "?" . $qs;
-    //$url = "https://testsbennert.cognitiveservices.azure.com/luis/prediction/v3.0/apps/fba6db0f-3d1d-42cd-8bda-488c14db85f6/slots/staging/predict?subscription-key=9692289c484a4b0aa0e673b3a7169d1f&verbose=true&show-all-intents=true&log=true&query=hi";
     print($url);
 
     // Perform the Web request and get the JSON response
     $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
+    return file_get_contents($url, false, $context);
 
-    return $result;
 }
 
 // check length of key
 if (strlen($endpointKey) == 32) {
 
-    print("LUIS Query: " . $term . "\n");
+    $strJsonFileContents = file_get_contents("clarabridge.json");
+    // Convert to array
+    $array = json_decode($strJsonFileContents, true);
 
+    for($i=0;$i < count($array); $i++){
 
-    $json = AnalyzeText($endpoint, $appId, $endpointKey, $term);
-
-    print("\nJSON Response:\n\n");
-    print(json_encode(json_decode($json), JSON_PRETTY_PRINT));
-    print("\n");
-    print($term);
-
-
+        $json = AnalyzeText($endpoint, $appId, $endpointKey, $array[$i]['title']);
+        print("\nJSON Response:\n\n");
+        print(json_encode(json_decode($json), JSON_PRETTY_PRINT));
+        print("\n");
+        print($term);
+        $fp = fopen('results.json', 'a');
+        fwrite($fp, json_encode(json_decode($json), JSON_PRETTY_PRINT));
+        fclose($fp);
+    }
 } else {
 
     print("Invalid LUIS key!\n");
